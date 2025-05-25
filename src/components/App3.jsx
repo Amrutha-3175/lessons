9e4bfebcf37f3a4cbb3b1e314cef6cc717dcbb80
Amrutha-3175 <23r11a0543@gcet.edu.in>
2025-05-25 17:12:55 +0530

    updated

--- src/components/App3.jsx
+++ src/components/App3.jsx
@@ -3,9 +3,10 @@ import Box from "./Box";
 export default function App3() {
   return (
     <div>
-      <h1>App3</h1>
+      <h1>App-3</h1>
       <h2>Conditional Rendering</h2>
       <Box flag={2} />
     </div>
+
   );
 }
